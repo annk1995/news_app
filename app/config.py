2840d4d5bef58e5943270ccdcc9ec2645e37bedc
annk1995 <kabuikariuki052@gmail.com>
2022-05-02 20:23:28 +0300

    created api base

--- app/config.py
+++ app/config.py
@@ -3,7 +3,7 @@ from instance.config import NEWS_API_KEY
 
 class Config:
   NEWS_API_KEY="7e46dfc39abd4a32a42ce973de9159e0"
-  MOVIE_API_BASE_URL ='https://newsapi.org/v2/everything?q=Apple&from=2022-05-02&sortBy=popularity&apiKey='+NEWS_API_KEY
+  NEWS_API_BASE_URL ='https://newsapi.org/v2/everything?q=Apple&from=2022-05-02&sortBy=popularity&apiKey='+NEWS_API_KEY
 class ProdConfig(Config):
     '''
     Production  configuration child class
--- app/request.py
+++ app/request.py
@@ -1,2 +1,45 @@
 from app import app
+from app.models.news_test import News
+
+import urllib.request,json
+from .models import news
+
+News=news.News
 api_key = app.config['NEWS_API_KEY']
+
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news():
+   
+    get_news_url = base_url.format(api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_response = json.loads(get_news_data)
+        print(get_news_response)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_response['results']
+            news_results = process_results(news_results_list)
+
+
+   
+
+    def process_results(get_news_response):
+        news_results = []
+    for news_item in get_news_response:
+        name = news_item.get('name')
+        author = news_item.get('author')
+        url = news_item.get('url')
+        urlToImage = news_item.get('urlToImage')
+        title = news_item.get('title')
+        description =news_item.get('description')
+        publishedAt = news_item.get('publishedAt')
+
+        if urlToImage and author and title:
+            news_object = news(author, title, description, url, urlToImage, publishedAt)
+            news_results.append(news_object)
+            
+    return news_results
