cb7d19272dc87f8d9ae47cc33d7cea818c9f5343
annk1995 <kabuikariuki052@gmail.com>
2022-05-03 01:08:18 +0300

    added sources

--- app/views.py
+++ app/views.py
@@ -1,12 +1,12 @@
 from flask import render_template
 from app import app
 # from .request import get_news
-from .request import get_headlines
+from .request import Headlines, get_headlines
 # Views
 
 @app.route('/')
 def index():
  news_headlines =get_headlines()
- return render_template('index.html',news=news_headlines)
+ return render_template('index.html',headlines=news_headlines)
     
     
