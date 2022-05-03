543f10a6fc2c0eab7a01606b7ebfec204048ae08
annk1995 <kabuikariuki052@gmail.com>
2022-05-03 12:29:45 +0300

    added styles

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from ensurepip import bootstrap
 from flask import Flask
 from .config import DevConfig
 
+
 from flask_bootstrap import Bootstrap
 # Initializing application
 app = Flask(__name__,instance_relative_config=True)
@@ -13,4 +14,5 @@ app.config.from_pyfile('config.py')
 bootstrap=Bootstrap(app)
 
 from app import views
+from app import error
 
